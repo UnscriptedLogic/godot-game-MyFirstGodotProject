2896f3eab58a42858eed37936e96317ea9f8e807
Nathaniel Alcala <semblancewarden@Gmail.com>
2023-09-21 13:15:20 +0800

    ⚡Frogs can move now

--- Characters/Frog/Frog.cs
+++ Characters/Frog/Frog.cs
@@ -3,50 +3,77 @@ using System;
 
 public partial class Frog : CharacterBody2D
 {
-    public const float JumpVelocity = -400.0f;
+    private const float JumpVelocity = -400.0f;
+    private const float Speed = 100f;
 
     private float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
     private AnimatedSprite2D animatedSprite;
+    private Timer chaseTimer;
 
+    private Vector2 prevPosition;
     private Node2D player;
 
     public override void _Ready()
     {
         animatedSprite = (AnimatedSprite2D)GetNode("AnimatedSprite2D");
+        chaseTimer = (Timer)GetNode("ChaseTimer");
+
+        animatedSprite.Play("Idle");
+        
+        prevPosition = Position;
     }
 
     public override void _PhysicsProcess(double delta)
     {
         Vector2 velocity = Velocity;
         velocity.Y += gravity * (float)delta;
-        Velocity = velocity;
 
         if (player != null)
         {
             Vector2 direction = (player.Position - Position).Normalized();
-
             if (direction.X > 0)
             {
                 //Right
                 animatedSprite.FlipH = true;
+                velocity.X = 1 * Speed;
             }
             else
             {
                 //Left
                 animatedSprite.FlipH = false;
+                velocity.X = -1 * Speed;
+            }
+
+            chaseTimer.Start();
+        }
+        else
+        {
+            if (chaseTimer.IsStopped())
+            {
+                velocity.X = 0;
+                chaseTimer.Stop();
             }
         }
 
+        Velocity = velocity;
         MoveAndSlide();
+
+        Animate();
     }
 
-    public void Jump()
+    private void Animate()
     {
-        if (IsOnFloor())
+        if ((prevPosition - Position).Length() > 0)
         {
-            velocity.Y = JumpVelocity;
-
+            GD.Print("Moving");
+            animatedSprite.Play("Jump");
         }
+        else
+        {
+            animatedSprite.Play("Idle");
+        }
+
+        prevPosition = Position;
     }
 
     public void OnBodyEntered(Node2D node)
